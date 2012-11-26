97449cb942ed59d93c77f00f9af8c799afe9933e
GastonRobledo <gastonrobledo@gmail.com>
2012-11-26 22:05:24 +0000

    Fix rotation positions

--- plugin/jquery.cropzoom.js
+++ plugin/jquery.cropzoom.js
@@ -386,8 +386,8 @@ THE SOFTWARE.
 														 */
 									$($image).css({
 										'msTransform' : rotacion,
-										'top' : getData('image').posY,
-										'left' : getData('image').posX
+										'top' : (getData('image').posY + (getData('image').h / 2)) + 'px',
+										'left' : (getData('image').posX + (getData('image').w / 2)) + 'px'
 									});
 
 								} else {
